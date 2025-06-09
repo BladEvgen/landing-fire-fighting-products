7dd066650c455e599e3dbcba50d97fea315ac0a5
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-06-09 20:23:15 +0500

    Create urls.py

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = "core"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("catalog/", views.catalog, name="catalog"),
+    path("product/<slug:slug>/", views.product_detail, name="product_detail"),
+    path("certificates/", views.certificates, name="certificates"),
+    path("about/", views.about, name="about"),
+    path("contacts/", views.contacts, name="contacts"),
+]
