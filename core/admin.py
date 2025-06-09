df09a088e0e6c5174efef7932fbf6808061e1180
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-06-09 20:24:23 +0500

    admin test version

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Tag,
+    Product,
+    Category,
+    Certificate,
+    CompanyInfo,
+    UserProfile,
+    ProductImage,
+    PasswordResetToken,
+    PasswordResetRequestLog,
+)
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+    list_display = ("title", "price")
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title",)
+    filter_horizontal = ("categories", "tags")
+
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Certificate)
+admin.site.register(CompanyInfo)
+admin.site.register(UserProfile)
+admin.site.register(PasswordResetToken)
+admin.site.register(PasswordResetRequestLog)
