49e88f0f577866dc8a67981189c3367feb01edb7
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-06-09 20:24:47 +0500

    added media root for test

--- firetech/urls.py
+++ firetech/urls.py
@@ -1,8 +1,14 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')), # grappelli URLS
+    path('grappelli/', include('grappelli.urls')), 
     path('admin/', admin.site.urls),
+    path("", include("core.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
