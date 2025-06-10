48d101927134222aa6efc6f8ab291b58bf38f7af
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-06-10 19:51:49 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,15 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("catalog/", views.catalog, name="catalog"),
-    path("product/<slug:slug>/", views.product_detail, name="product_detail"),
-    path("certificates/", views.certificates, name="certificates"),
-    path("about/", views.about, name="about"),
-    path("contacts/", views.contacts, name="contacts"),
+    path('', views.index, name='index'),
+    
+    path('catalog/', views.catalog, name='catalog'),
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('category/<slug:slug>/', views.category_detail, name='category_detail'),
+        
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('certificates/', views.certificates, name='certificates'),
+    
+    path('search/', views.search_results, name='search_results'),
 ]
