010f3fb677c1522289dac8678b71870080a3e1cd
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-06-12 20:52:37 +0500

    black

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path
 
-from . import views
+from core import views
 
 app_name = "core"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    
-    path('catalog/', views.catalog, name='catalog'),
-    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('category/<slug:slug>/', views.category_detail, name='category_detail'),
-        
-    path('about/', views.about, name='about'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('certificates/', views.certificates, name='certificates'),
-    
-    path('search/', views.search_results, name='search_results'),
+    path("", views.index, name="index"),
+    path("catalog/", views.catalog, name="catalog"),
+    path("product/<slug:slug>/", views.product_detail, name="product_detail"),
+    path("category/<slug:slug>/", views.category_detail, name="category_detail"),
+    path("about/", views.about, name="about"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("certificates/", views.certificates, name="certificates"),
+    path("search/", views.search_results, name="search_results"),
 ]
