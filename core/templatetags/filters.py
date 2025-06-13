40b75c01d636affe562b4c2ed604a2c5499e322a
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-06-13 19:45:15 +0500

    Update filters.py

--- core/templatetags/filters.py
+++ core/templatetags/filters.py
@@ -8,3 +8,10 @@ def custom_cut(text, length):
     if len(str(text)) > length:
         return str(text)[:length] + "..."
     return str(text)
+
+
+@register.filter
+def chunked(lst, chunk_size):
+    chunk_size = int(chunk_size)
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i:i + chunk_size]
